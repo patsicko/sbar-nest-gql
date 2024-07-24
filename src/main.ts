61dd127ea77148aeaa053f285ef59af82d8b976d
patsicko <patsicko@gmail.com>
2024-07-24 08:15:26 +0200

    update cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:4200",
+    origin: "https://mybrand-ng-fe.vercel.app/",
     credentials: true
   });
   app.use(cookieParser());
