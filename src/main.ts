2d7ebfe3c3017397835f7f2e64896bc4751a3cf7
patsicko <patsicko@gmail.com>
2024-08-02 09:13:00 +0200

    deploy to render

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
+// import { NestFactory } from "@nestjs/core";
+// import { AppModule } from "./app.module";
+// import "dotenv/config";
+// import { ValidationPipe } from "@nestjs/common";
+// import * as cookieParser from "cookie-parser";
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors({
+//     origin: "http://localhost:4200",
+//     credentials: true
+//   });
+//   app.use(cookieParser());
+//   app.useGlobalPipes(new ValidationPipe());
+//   await app.listen(process.env.PORT);
+// }
+// bootstrap();
+
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import "dotenv/config";
@@ -7,7 +25,7 @@ import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:4200",
+    origin: "https://sbar-apollo-angular.vercel.app",
     credentials: true
   });
   app.use(cookieParser());
@@ -15,3 +33,4 @@ async function bootstrap() {
   await app.listen(process.env.PORT);
 }
 bootstrap();
+
