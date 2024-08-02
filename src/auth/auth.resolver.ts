60f1d21f2ebe5bbfdf29896e4465ca79401530f6
patsicko <patsicko@gmail.com>
2024-08-02 09:34:35 +0200

    configure cors

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -21,8 +21,10 @@ export class AuthResolver {
     const { accessToken } = await this.authService.login(user);
 
     context.res.cookie("accessToken", accessToken, {
-      sameSite: "strict",
-      maxAge: 24 * 60 * 60 * 1000
+      httpOnly: true,       // Ensure the cookie is only accessible by the web server
+      sameSite: "strict",   // 'strict' is fine, but you might need 'lax' depending on your frontend requests
+      secure: true,         // Ensure the cookie is only sent over HTTPS
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
     return { accessToken };
