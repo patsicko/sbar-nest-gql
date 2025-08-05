8944fc0837a6ba0168b27da721d779a161a37c8e
MANIBAHO Patrick <patsicko@gmail.com>
2025-08-05 13:04:52 +0200

    prod

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,8 @@ import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://sbar-apollo-angular.vercel.app",
+    // origin: "https://sbar-apollo-angular.vercel.app",
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:4200",
     credentials: true
   });
   app.use(cookieParser());
